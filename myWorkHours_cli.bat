1402dae1a2cd840607504045be1ccef9baf224e9
em7 <mm007.emko@gmail.com>
2013-07-09 23:08:35 +0200

    The windows batch file over command line interface

---
+++ myWorkHours_cli.bat
@@ -0,0 +1,14 @@
+@echo off
+rem the script to show console and run the myWorkHours_cli, then wait
+rem should have the .py extension associated with pythonw.exe
+
+echo USAGE: past the work hour report copied from the WebTerm website
+echo one per line. When done with the pasting, press ENTER key and then
+echo CTRL+Z and ENTER again (= the end of standard input
+echo in DOS/Windows environment).
+echo The actual time is printed. 
+echo.
+echo If the LEAVE is not the last event, it will count the time in work till NOW.
+
+myWorkHours_cli.py
+@pause
