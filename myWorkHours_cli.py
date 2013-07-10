19631c2b44150ffd196db3bd916a1c717ec41bb2
em7 <mm007.emko@gmail.com>
2013-07-10 07:42:41 +0200

    added license terms

--- myWorkHours_cli.py
+++ myWorkHours_cli.py
@@ -17,6 +17,8 @@ def print_help():
     """
     print("""myWorkHours_cli is a CLI for a program for counting the
 time spent in work. Uses the format used by WebTerm software.
+version 1.1
+distributed under the terms and conditions of WTFPL www.wtfpl.net
 
 usage: myWork [arg]
 where: arg can be   -h or --help or /? for this help
