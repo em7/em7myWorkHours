9ab3ec25e0ea535308f0e4fae4ab6553658aa461
em7 <mm007.emko@gmail.com>
2013-07-10 07:47:00 +0200

    added license terms

--- myWorkHours_cli.bat
+++ myWorkHours_cli.bat
@@ -2,6 +2,9 @@
 rem the script to show console and run the myWorkHours_cli, then wait
 rem should have the .py extension associated with pythonw.exe
 
+echo myWorkHours version 1.0.1
+echo distributed under the terms and conditions of WTFPL www.wtfpl.net
+echo.
 echo USAGE: past the work hour report copied from the WebTerm website
 echo one per line. When done with the pasting, press ENTER key and then
 echo CTRL+Z and ENTER again (= the end of standard input
