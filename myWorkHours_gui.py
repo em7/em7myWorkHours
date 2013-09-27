826f9ccd148a27c9cdf2c5052fef7e2846f85e7d
em7 <mm007.emko@gmail.com>
2013-09-27 09:56:36 +0200

    created the main gui window

--- myWorkHours_gui.py
+++ myWorkHours_gui.py
@@ -1,34 +1,70 @@
 import tkinter as tk
+import tkinter.ttk as ttk
+import tkinter.scrolledtext as tkst
 
-#tkdocs.com
 
-class Application(tk.Frame):
+class MyWorkHoursMainWin(ttk.Frame):
     def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
-        self.pack()
+        ttk.Frame.__init__(self, master)
+        if not (self.master == None):
+            self.master.title("Hello")
+            
+        self.pack(fill='both', expand='yes', padx=5, pady=5)
         self.createWidgets()
+        self.createMenu()
 
     def createWidgets(self):
-        self.hel_lbl = tk.Label(self)
-        self.hel_lbl["text"] = u"HelloWorld"
-        self.hel_lbl.grid(column=0,row=0,sticky='NEW')
         
+        self.hours_label = ttk.Label(self)
+        self.hours_label["text"] = u"Please paste the work hour report copied from\n" \
+                + "the WebTerm website, one per line\n" \
+                + "(= copied from Chrome or IE, Firefox puts them on different lines)."
+        self.hours_label.grid(row=0,column=0,sticky='W',columnspan=4)
         
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.grid(column=0,row=1)
+        self.hours_scrollbar = ttk.Scrollbar(self)
+        self.hours_edit = tk.Text(self, width=10, height=10, wrap='word',
+                                  yscrollcommand=self.hours_scrollbar.set)
+        self.hours_scrollbar.config(command=self.hours_edit.yview)
+        self.hours_edit.grid(row=1,column=0,columnspan=3,sticky='NEWS',
+                             pady=5)
+        self.hours_scrollbar.grid(row=1,column=3,sticky='NSE',pady=5)
+        
+        self.time_label = ttk.Label(self)
+        self.time_label["text"] = u"Computed time:"
+        self.time_label.grid(row=2,column=0,sticky='W')
 
-        self.QUIT = tk.Button(self, text="QUIT", fg="red",
-                                            command=root.destroy)
-        self.QUIT.grid(column=0,row=2)
+        self.time_comp = ttk.Label(self)
+        self.time_comp["text"] = u"comptime:0"
+        self.time_comp.grid(row=2,column=1,sticky='W', padx=5)
+        
+        self.compute_button = ttk.Button(self)
+        self.compute_button["text"] = "Compute"
+        self.compute_button["command"] = self.compute
+        self.compute_button.grid(row=2,column=2,columnspan=2,sticky='W')
 
-        self.grid_columnconfigure(0,weight=1)
-        self.grid_rowconfigure(0,weight=1)
+        self.grid_columnconfigure(1,weight=2)
+        self.grid_columnconfigure(2,weight=1)
+        self.grid_rowconfigure(1,weight=1)
+        
+    def createMenu(self):
+        self.menubar = tk.Menu(self)
+        
+        self.myWorkHours_menu = tk.Menu(self.menubar, tearoff=0)
+        self.myWorkHours_menu.add_command(label="About", command=self.about)
+        self.myWorkHours_menu.add_separator()
+        self.myWorkHours_menu.add_command(label="Exit", command=root.destroy)
+        
+        self.menubar.add_cascade(label="Program", menu=self.myWorkHours_menu)
+        
+        root.config(menu=self.menubar)
 
-    def say_hi(self):
-        print("hi there, everyone!")
+    def compute(self):
+        tl = tk.Toplevel(self)
+        
+    def about(self):
+        tl = tk.Toplevel(self)
 
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = MyWorkHoursMainWin(master=root)
+    app.mainloop()
